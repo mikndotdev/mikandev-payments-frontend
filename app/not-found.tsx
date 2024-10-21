5bde554203672080da9ff1eb81693da01263cb77
maamokun <maamo-kun@outlook.com>
2024-10-21 10:30:16 +0900

    not found page with edge

---
+++ app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export const runtime = "edge";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
+}
