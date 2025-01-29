782bab6bd106cb47770a6e9f81fff0e4f249bd8e
maamokun/MikanDev <maamo-kun@outlook.com>
2025-01-29 09:36:22 +0900

    feat: UID metadata for payments

--- app/ui/payments/createPayment.tsx
+++ app/ui/payments/createPayment.tsx
@@ -1,11 +1,16 @@
 "use server";
 import { api } from "@/polar";
+import { auth } from "@/auth";
 
 export async function createPayment(id: string, email: string) {
+    const session = await auth();
     const res = await api.checkouts.custom.create({
         productId: id,
         embedOrigin: process.env.NEXTAUTH_URL,
         customerEmail: email,
+        metadata: {
+            userId: session?.user?.id || "",
+        }
     });
 
     return res.url;
